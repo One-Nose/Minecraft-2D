d328668008b200e4f0cd3c04e32528c145d63461
One Nose <idohaolam@gmail.com>
2023-02-14 21:53:05 +0200

    Added sky

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Application } from 'pixi.js'
 
-
 const canvas = document.getElementById('game') as HTMLCanvasElement
 
 const app = new Application({
+    backgroundColor: 0x78A7FF,
     resizeTo: canvas,
     view: canvas,
 })
