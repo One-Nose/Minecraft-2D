e0093512a0850c4d1d91d71a8dedbfd3e691347e
One Nose <idohaolam@gmail.com>
2023-02-17 01:46:24 +0200

    Added 3D effect

--- src/world/block.ts
+++ src/world/block.ts
@@ -1,5 +1,5 @@
 import { BlockRegistry, blockRegistry } from "blockRegistry"
-import { Sprite, Texture } from "pixi.js"
+import { Container, Sprite, Texture } from "pixi.js"
 import Chunk from "./chunk"
 import Row from "./row"
 
@@ -13,20 +13,29 @@ export default class Block {
     /** The block registry */
     block: BlockRegistry
 
-    /** The block's location within the row */
-    x: number
+    /** The block's front sprite */
+    front: Sprite
+
+    /** A container for the block's sprites */
+    container: Container
 
     /** `true` if the block is shadowed */
     isDark: boolean
-
+    
     /** `true` if the block's sprite is loaded */
     isLoaded: boolean
-
+    
     /** The block's row */
     row: Row
 
-    /** The block's sprite */
-    sprite: Sprite
+    /** The block's right sprite */
+    side: Sprite
+
+    /** The block's top sprite */
+    top: Sprite
+
+    /** The block's location within the row */
+    x: number
 
     /**
      * @param row The block's row
@@ -40,9 +49,27 @@ export default class Block {
         this.isLoaded = false
         this.row = row
 
-        this.sprite = new Sprite()
-        this.sprite.x = x * Block.SIZE
-        this.sprite.y = (Chunk.HEIGHT - row.y - 1) * Block.SIZE
+        this.container = new Container()
+        this.container.x = x * Block.SIZE
+        this.container.y = (Chunk.HEIGHT - row.y - 1) * Block.SIZE
+
+        this.front = new Sprite()
+        this.container.addChild(this.front)
+
+        this.top = new Sprite()
+        this.side = new Sprite()
+
+        this.top.anchor.y = 1
+        this.side.x = Block.SIZE
+
+        this.side.skew.y = -Math.PI / 4
+        this.top.skew.x = -(Math.PI / 2 + this.side.skew.y)
+
+        this.top.height = Block.SIZE / 2
+        this.side.width = this.top.height
+
+        this.container.addChild(this.top)
+        this.container.addChild(this.side)
     }
 
     /**
@@ -51,7 +78,7 @@ export default class Block {
     load(): void {
         if (!this.isLoaded) {
             if (this.block.isVisible) {
-                this.row.chunk.container.addChild(this.sprite)
+                this.row.chunk.container.addChild(this.container)
             }
             this.isLoaded = true
         }
@@ -62,18 +89,24 @@ export default class Block {
      */
     update(): void {
         if (this.block.isVisible) {
-            this.sprite.visible = true
-            this.sprite.texture = this.block.texture
-
             if (this.row.chunk.heights[this.x] === this.row.y) {
                 this.isDark = false
             } else {
                 this.isDark = true
             }
-            this.sprite.tint = this.isDark ? 0x888888 : 0xFFFFFF
-        } else {
-            this.sprite.texture = Texture.EMPTY
-            this.sprite.visible = false
+
+            this.front.tint = 0xFFFFFF
+            this.top.tint = 0xBBBBBB
+            this.side.tint = 0xCCCCCC
+
+            if (this.isDark) {
+                this.front.tint -= 0x444444
+                this.top.tint -= 0x444444
+                this.side.tint -= 0x444444
+            }
+
+            this.top.visible = !this.row.chunk.rows[this.row.y + 1]?.blocks[this.x]?.block?.isVisible
+            this.side.visible = !this.row.blocks[this.x + 1]?.block?.isVisible
         }
     }
 
@@ -85,9 +118,21 @@ export default class Block {
         this.block = blockRegistry[id]
 
         if (this.block.isVisible) {
+            this.container.visible = true
+
+            this.front.texture = this.block.texture
+            this.top.texture = this.block.texture
+            this.side.texture = this.block.texture
+
             if (this.row.y > this.row.chunk.heights[this.x]) {
                 this.row.chunk.heights[this.x] = this.row.y
             }
+        } else {
+            this.container.visible = false
+
+            this.front.texture = Texture.EMPTY
+            this.top.texture = Texture.EMPTY
+            this.side.texture = Texture.EMPTY
         }
     }
 }
