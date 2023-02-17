1a43610300f0ac628da26177a330fd7e38e71a88
One Nose <idohaolam@gmail.com>
2023-02-17 16:05:45 +0200

    Added gravity, though accidentally upside down

--- src/player.ts
+++ src/player.ts
@@ -7,6 +7,9 @@ import World from "world/world"
  * Represents a player
  */
 export default class Player {
+    /** The player's current motion vector */
+    motion: {x: number, y: number}
+
     /** The player's sprite */
     sprite: Sprite
 
@@ -32,6 +35,7 @@ export default class Player {
         this.sprite.y = Block.SIZE - this.sprite.x
         app.stage.addChild(this.sprite)
 
+        this.motion = {x: 0, y: 0}
         this.world = world
         this.x = x
         this.y = y
@@ -42,23 +46,84 @@ export default class Player {
     }
 
     /**
-     * Moves the player towards a location
-     * @param x The relative X of the new location
-     * @param y The relative Y of the new location
+     * Moves the player according to it's `motion`
      */
-    move(x: number, y: number): void {
+    move(): void {
+        let minX, maxX, minY, maxY
+
+        if (this.motion.x > 0) {
+            minX = -Infinity
+            maxX = this.x + this.motion.x * Block.SIZE
+        } else if (this.motion.x < 0) {
+            minX = this.x + this.motion.x * Block.SIZE
+            maxX = Infinity
+        } else {
+            minX = -Infinity
+            maxX = Infinity
+        }
+
+        if (this.motion.y > 0) {
+            minY = this.y - this.motion.y * Block.SIZE
+            maxY = Infinity
+        } else if (this.motion.y < 0) {
+            minY = Infinity
+            maxY = this.y - this.motion.y * Block.SIZE
+        } else {
+            minY = -Infinity
+            maxY = Infinity
+        }
+
         let currentX = this.x
         let currentY = this.y
-        while (currentX <= this.x + x && currentY <= this.y + y) {
-            // TODO
-            break
+
+        // console.log(
+        //     `currentX: ${currentX}\n` +
+        //     `currentY: ${currentY}\n` +
+        //     `minX: ${minX}\n` +
+        //     `maxX: ${maxX}\n` +
+        //     `minY: ${minY}\n` +
+        //     `maxY: ${maxY}`
+        // )
+        // return
+        while (minX <= currentX && currentX <= maxX && minY <= currentY && currentY <= maxY) {
+            let xDistance, yDistance
+
+            if (this.motion.x > 0) {
+                xDistance = Block.SIZE - currentX % Block.SIZE
+            } else if (this.motion.x < 0) {
+                xDistance = currentX % Block.SIZE + 1
+            } else xDistance = Infinity
+
+            if (this.motion.y > 0) {
+                yDistance = -(currentY % Block.SIZE + 1)
+            } else if (this.motion.y < 0) {
+                yDistance = Block.SIZE - currentY % Block.SIZE
+            } else yDistance = Infinity
+
+            if (xDistance < yDistance) {
+                yDistance = -xDistance * this.motion.y / this.motion.x
+            } else if (yDistance < xDistance) {
+                xDistance = -yDistance * this.motion.x / this.motion.y
+            }
+
+            if (this.world.getBlock(currentX + xDistance, currentY + yDistance)?.block?.isVisible) {
+                this.x = currentX + xDistance
+                this.y = currentY + yDistance
+                break
+            }
         }
+        if (this.motion.x > 0) this.x = maxX
+        else if (this.motion.x < 0) this.x = minX
+
+        if (this.motion.y > 0) this.y = minY
+        else if (this.motion.y < 0) this.y = maxY
     }
 
     /**
      * Runs every tick
      */
     tick(): void {
-        this.move(0, -1)
+        this.motion.y = -0.001
+        this.move()
     }
 }
