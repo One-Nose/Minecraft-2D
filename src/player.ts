604d109ee62549a79fd3b6a9f0cb2b6767fbbd39
One Nose <idohaolam@gmail.com>
2023-07-01 22:42:56 +0300

    fix(player): motion not working properly

--- src/player.ts
+++ src/player.ts
@@ -48,119 +48,210 @@ export default class Player {
     }
 
     /**
+     * Gets the Y value of the most bottom part of the player
+     * @returns The Y value of that part
+     */
+    bottom(): number {
+        return this.y
+    }
+
+    /**
+     * Gets the farthest Y value to the player's bottom
+     * that is within the same block as the player
+     * @returns The Y value of the bottom block edge
+     */
+    bottomBlockEdge(): number {
+        return Math.floor(this.bottom())
+    }
+
+    /**
+     * Checks if there is a solid block right to the player's bottom
+     * @returns Whether there is a solid block there
+     */
+    isBottomSolid(): boolean {
+        const rightX = this.right()
+        const blockY = this.bottom() - 0.001
+
+        let blockX = Math.floor(this.left())
+        while (blockX <= rightX) {
+            if (this.world.getBlock(blockX, blockY)?.block.isSolid) return true
+            blockX++
+        }
+        return false
+    }
+
+    /**
+     * Checks if there is a solid block right to the player's left
+     * @returns Whether there is a solid block there
+     */
+    isLeftSolid(): boolean {
+        const topY = this.top()
+        const blockX = this.left() - 0.001
+
+        let blockY = Math.floor(this.bottom())
+        while (blockY <= topY) {
+            if (this.world.getBlock(blockX, blockY)?.block.isSolid) return true
+            blockY++
+        }
+        return false
+    }
+
+    /**
+     * Checks if there is a solid block right to the player's right
+     * @returns Whether there is a solid block there
+     */
+    isRightSolid(): boolean {
+        const topY = this.top()
+        const blockX = this.right() + 0.001
+
+        let blockY = Math.floor(this.bottom())
+        while (blockY <= topY) {
+            if (this.world.getBlock(blockX, blockY)?.block.isSolid) return true
+            blockY++
+        }
+        return false
+    }
+
+    /**
+     * Checks if there is a solid block right to the player's top
+     * @returns Whether there is a solid block there
+     */
+    isTopSolid(): boolean {
+        const rightX = this.right()
+        const blockY = this.top() + 0.001
+
+        let blockX = Math.floor(this.left())
+        while (blockX <= rightX) {
+            if (this.world.getBlock(blockX, blockY)?.block.isSolid) return true
+            blockX++
+        }
+        return false
+    }
+
+    /**
+     * Gets the X value of the left part of the player
+     * @returns The X value of that part
+     */
+    left(): number {
+        return this.x - Player.WIDTH / 2
+    }
+
+    /**
+     * Gets the farthest X value to the player's left
+     * that is within the same block as the player
+     * @returns The X value of the left block edge
+     */
+    leftBlockEdge(): number {
+        return Math.floor(this.left())
+    }
+
+    /**
      * Moves the player according to it's `motion` vector
      */
     move(): void {
-        let minX: number, maxX: number, minY: number, maxY: number
-
-        if (this.motion.x > 0) {
-            minX = -Infinity
-            maxX = this.x + this.motion.x
-        } else if (this.motion.x < 0) {
-            minX = this.x + this.motion.x
-            maxX = Infinity
-        } else {
-            minX = -Infinity
-            maxX = Infinity
-        }
-
-        if (this.motion.y > 0) {
-            minY = -Infinity
-            maxY = this.y + this.motion.y
-        } else if (this.motion.y < 0) {
-            minY = this.y + this.motion.y
-            maxY = Infinity
-        } else {
-            minY = -Infinity
-            maxY = Infinity
-        }
-
-        minX = Number(minX.toPrecision(12))
-        minY = Number(minY.toPrecision(12))
-
-        let currentX = this.x
-        let currentY = this.y
-        let isXBlocked = false
-        let isYBlocked = false
+        const originalX = this.x
+        const originalY = this.y
+        const originalMotion = this.motion
 
         while (
-            minX <= currentX &&
-            currentX <= maxX &&
-            minY <= currentY &&
-            currentY <= maxY
+            Math.abs(this.x - originalX) <= Math.abs(originalMotion.x) &&
+            Math.abs(this.y - originalY) <= Math.abs(originalMotion.y)
         ) {
-            let deltaX = Infinity,
-                deltaY = Infinity
+            const isXSolid =
+                this.motion.x === 0
+                    ? false
+                    : this.motion.x > 0
+                    ? this.isRightSolid()
+                    : this.isLeftSolid()
 
-            if (!isXBlocked) {
-                if (this.motion.x > 0) {
-                    deltaX = 1 - (currentX - Math.floor(currentX))
-                } else if (this.motion.x < 0) {
-                    deltaX = -(currentX - Math.floor(currentX) + 0.001)
-                }
-                deltaX = Number(deltaX.toPrecision(12))
-            }
+            const isYSolid =
+                this.motion.y === 0
+                    ? false
+                    : this.motion.y > 0
+                    ? this.isTopSolid()
+                    : this.isBottomSolid()
 
-            if (!isYBlocked) {
-                if (this.motion.y > 0) {
-                    deltaY = 1 - (currentY - Math.floor(currentY))
-                } else if (this.motion.y < 0) {
-                    deltaY = -(currentY - Math.floor(currentY) + 0.001)
-                }
-                deltaY = Number(deltaY.toPrecision(12))
-            }
+            if (isXSolid) this.motion.x = 0
+            else this.x += Math.sign(this.motion.x) * 0.001
 
-            let closest: 'x' | 'y' | 'none'
-            if (Math.abs(deltaX) < Math.abs(deltaY)) {
-                closest = 'x'
-                deltaY = (deltaX * this.motion.y) / this.motion.x
-            } else if (Math.abs(deltaY) < Math.abs(deltaX)) {
-                closest = 'y'
-                deltaX = (deltaY * this.motion.x) / this.motion.y
-            } else if (deltaX === Infinity && deltaY === Infinity) {
-                break
-            } else closest = 'none'
+            if (isYSolid) this.motion.y = 0
+            else this.y += Math.sign(this.motion.y) * 0.001
 
-            const xBeforeChange = currentX
-            const yBeforeChange = currentY
-            if (
-                this.world.getBlock(currentX + deltaX, currentY)?.block?.isSolid
-            ) {
-                if ((closest === 'x' || closest === 'none') && !isXBlocked) {
-                    currentX = Math.max(minX, Math.min(maxX, currentX + deltaX))
-                    if (this.motion.x > 0) currentX -= 0.001
-                    else if (this.motion.x < 0) currentX += 0.001
-                    currentX = Number(currentX.toPrecision(12))
-                    this.x = currentX
-                    isXBlocked = true
-                }
-            }
-            if (
-                this.world.getBlock(currentX, currentY + deltaY)?.block?.isSolid
-            ) {
-                if ((closest === 'y' || closest === 'none') && !isYBlocked) {
-                    currentY = Math.max(minY, Math.min(maxY, currentY + deltaY))
-                    if (this.motion.y > 0) currentY -= 0.001
-                    else if (this.motion.y < 0) currentY += 0.001
-                    currentY = Number(currentY.toPrecision(12))
-                    this.y = currentY
-                    isYBlocked = true
-                }
-            }
-            if (isXBlocked && isYBlocked) break
-
-            currentX = Number((xBeforeChange + deltaX).toPrecision(12))
-            currentY = Number((yBeforeChange + deltaY).toPrecision(12))
-        }
-        if (!isXBlocked) {
-            if (this.motion.x > 0) this.x = maxX
-            else if (this.motion.x < 0) this.x = minX
+            if (this.motion.x === 0 && this.motion.y === 0) return
+            ;({ x: this.x, y: this.y } = this.nextBlockEdge())
         }
 
-        if (!isYBlocked) {
-            if (this.motion.y > 0) this.y = maxY
-            else if (this.motion.y < 0) this.y = minY
+        this.x = this.motion.x === 0 ? this.x : originalX + originalMotion.x
+        this.y = this.motion.y === 0 ? this.y : originalY + originalMotion.y
+    }
+
+    /**
+     * Gets the farthest point the player would move to,
+     * that is still within the same block, according to their motion
+     *
+     * @returns The X and Y values of that point
+     */
+    nextBlockEdge(): { x: number; y: number } {
+        const { x: nextX, y: nextY } = this.farthestInSameBlock()
+
+        if (nextX === null && nextY === null) return { x: this.x, y: this.y }
+
+        if (nextX === null && nextY !== null) return { x: this.x, y: nextY }
+
+        if (nextY === null && nextX !== null) return { x: nextX, y: this.y }
+
+        if (nextX !== null && nextY !== null) {
+            const xyRatio = Math.abs(this.motion.x / this.motion.y)
+            const distanceX = nextX - this.x
+            const distanceY = nextY - this.y
+
+            return Math.abs(distanceX) < Math.abs(distanceY) * xyRatio
+                ? { x: nextX, y: this.y + distanceX / xyRatio }
+                : { x: this.x + distanceY * xyRatio, y: nextY }
         }
+
+        throw 'Unreachable code'
+    }
+
+    /**
+     * Gets the X value of the farthest point the player can reach horizontally
+     * and the Y value of the farthest point the player can reach vertically
+     * in the direction of their motion, while remaining in the same block
+     *
+     * @returns The X and Y values of the two points
+     */
+    farthestInSameBlock(): { x: number | null; y: number | null } {
+        return {
+            x:
+                this.motion.x === 0
+                    ? null
+                    : this.motion.x > 0
+                    ? this.rightBlockEdge() - Player.WIDTH / 2 + 0.001
+                    : this.leftBlockEdge() + Player.WIDTH / 2,
+            y:
+                this.motion.y === 0
+                    ? null
+                    : this.motion.y > 0
+                    ? this.topBlockEdge() - Player.HEIGHT + 0.001
+                    : this.bottomBlockEdge(),
+        }
+    }
+
+    /**
+     * Gets the X value of the right part of the player
+     * @returns The X value of that part
+     */
+    right(): number {
+        return this.x + Player.WIDTH / 2 - 0.001
+    }
+
+    /**
+     * Gets the farthest X value to the player's right
+     * that is within the same block as the player
+     * @returns The X value of the right block edge
+     */
+    rightBlockEdge(): number {
+        return Math.floor(this.right()) + 0.999
     }
 
     /**
@@ -169,5 +260,22 @@ export default class Player {
     tick(): void {
         this.motion.y = -0.1
         this.move()
+    }
+
+    /**
+     * Gets the Y value of the top part of the player
+     * @returns The Y value of that part
+     */
+    top(): number {
+        return this.y + Player.HEIGHT - 0.001
+    }
+
+    /**
+     * Gets the farthest Y value to the player's top
+     * that is within the same block as the player
+     * @returns The Y value of the top block edge
+     */
+    topBlockEdge(): number {
+        return Math.floor(this.top()) + 0.999
     }
 }
